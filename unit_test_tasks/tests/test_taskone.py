4fdd4de6bdd820dbed03cf24580440f0ed18ab52
Andrius Sabaliauskas <124807066+AndriusSab@users.noreply.github.com>
2023-05-09 19:50:37 +0300

    adding smth

---
+++ unit_test_tasks/tests/test_taskone.py
@@ -0,0 +1,8 @@
+import unittest
+import taskone
+
+
+class TestSuma(unittest.TestCase):
+    def test_skaiciu_suma(self):
+        self.assertEqual(9, taskone.skaiciu_suma(9, 3, 4))
+        # self.assertNotEqual(taskone.skaiciu_suma(1, 1, 1), 0)
